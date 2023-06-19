bdec60168ac0af5e159d736d001da3de5b820a95
pi-tronic <robert@mi-s.de>
2023-06-19 18:41:53 +0200

    try to fix linked list

--- two-pass algorithm/color2.py
+++ two-pass algorithm/color2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import get_plot_commands
 import numpy as np
 from PIL import Image
 
@@ -10,9 +11,9 @@ map_r = 1.9
 
 threshold = 10
 background = [0., 0., 0.]
-center = [1., .0, .0]
-bot = [.0, 1., .0]
+center = [1., 1., 1.]
 waypoint = [.0, .0, 1.]
+colors = [[.69, .15, .4], [1., .2, .43], [1., .74, .4], [1., .42, .3], [.0, .53, .6], [.24, .51, .38], [1., .9, .41], [.88, .67, .35], [.72, .58, .82], [1., .0, .0], [.0, 1., .0], [.0, .0, 1.], [1., 1., .0], [1., .0, 1.], [.0, 1., 1.]]
 adjacent1 = [[0,-1], [-1,0], [-1,-1], [-1,1]]    # W, N, NW, NE
 adjacent2 = [[0,1], [1,0], [1,-1], [1,1]]    # O, S, SW, SE
 
@@ -20,27 +21,24 @@ src_img = np.array(Image.open('construction_map.pgm'))
 M = len(src_img)
 N = len(src_img[0])
 
+print(M, N)
+
 def two_pass(data):
     linked = []
     labels = np.zeros((M,N))
     nextLabel = 1
 
     # first pass
-    for row in range(len(data)):
-        for column in range(len(data[row])):
+    for row in range(M):
+        for column in range(N):
             # if not background
             if data[row][column].tolist() != background:
                 # get all neighbors (W, N, NW, NE)
                 neighbors = []
-                neighbors_labels = []
                 for direction in adjacent1:
-                    try:
+                    if row+direction[0] in range(M) and column+direction[1] in range(N):
                         if data[row+direction[0]][column+direction[1]].tolist() != background:
-                            neighbors.append(data[row+direction[0]][column+direction[1]])
-                            neighbors_labels.append(labels[row+direction[0]][column+direction[1]])
-                    except:
-                        print('doesnt exist! out of image!')
-
+                            neighbors.append(labels[row+direction[0]][column+direction[1]])
                 # create new label if no neighbors found
                 if len(neighbors) == 0:
                     linked.append([nextLabel])
@@ -49,11 +47,10 @@ def two_pass(data):
 
                 else:
                     # find smallest label of neighbors
-                    L = neighbors_labels
-                    labels[row][column] = min(L)
-                    for label in L:
-                        linked[int(label)-1].append(nextLabel-1)
-
+                    labels[row][column] = int(min(neighbors))
+                    for label in neighbors:
+                        linked[nextLabel-2].append(int(label))
+            
     # second pass
     for i in range(len(linked)):
         current_label = linked[i][0]
@@ -81,6 +78,8 @@ def find_coords(data, labels, x1, y1, x2, y2):
     coords = []
     for i in range(labels):
         coords.append([])
+
+    print(len(coords))
 
     # get all the same labels coords together
     for row in range(len(data)):
@@ -157,24 +156,30 @@ def main():
     # apply two-pass algorithm (label connected areas)
     label_img, label_amount = two_pass(gray_img)
 
+    # colorize labels to be distinguishable
+    col_img = np.zeros((M,N,3))
+    for label in range(1, label_amount):
+        col_img[label_img==label] = colors[randint(0, len(colors)-1)]
+
     # calculate coords of the robot for matplotlib coordinate system, also visualize them
     robot_x = (robot_position[0] + map_r) / (2 * map_r) * N
     robot_y = (-robot_position[1] + map_r) / (2 * map_r) * M
-    gray_img[int(robot_y)][int(robot_x)] = bot
+    col_img[int(robot_y)][int(robot_x)] = center
 
     # paint center coords on top (white)
-    coords = find_coords(label_img, label_amount, robot_x, 0, N, robot_y)   # check in construction site
+    # coords = find_coords(label_img, label_amount, robot_x, 0, N, robot_y)   # check in construction site
+    coords = find_coords(label_img, label_amount, 0, 0, N, M)
     for coord in coords:
         print(coord)
-        gray_img[coord[0]][coord[1]] = center
+        col_img[coord[0]][coord[1]] = center
 
     # set and visualize waypoints
     waypoints = create_waypoints(coords, robot_x, robot_y)
     for wp in waypoints:
-        gray_img[int(wp[0])][int(wp[1])] = waypoint
+        col_img[int(wp[0])][int(wp[1])] = center
 
     # show image
-    plt.imshow(gray_img, interpolation='None')
+    plt.imshow(col_img, interpolation='None')
     plt.show()
 
 main()
